6c9b319ea9383139c9c3bb8952ad72161ae73121
feezahlee <hafeezahlee3@gmail.com>
2024-08-14 12:53:22 +0800

    Update apache_secure2.py

--- apache_secure2.py
+++ apache_secure2.py
@@ -1,101 +1,52 @@
 import subprocess
-
-container_name = "clab-firstlab-apache-server"
-
-def run_command(command, check=True):
-    try:
-        result = subprocess.run(command, shell=True, check=check, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return result.stdout.decode(), result.stderr.decode()
-    except subprocess.CalledProcessError as e:
-        print(f"Command '{command}' failed with error: {e.stderr.decode()}")
-        return None, e.stderr.decode()
+import fileinput
+import shutil
 
 def rename_and_move_certificates():
-    print("Renaming and moving SSL certificate and key...")
-    stdout, stderr = run_command(f'docker exec {container_name} cp /usr/local/apache2/conf/ssl/apache.crt /usr/local/apache2/conf/server.crt')
-    if stdout:
-        print(stdout)
-    if stderr:
-        print(stderr)
-    stdout, stderr = run_command(f'docker exec {container_name} cp /usr/local/apache2/conf/ssl/apache.key /usr/local/apache2/conf/server.key')
-    if stdout:
-        print(stdout)
-    if stderr:
-        print(stderr)
+    shutil.copy('/usr/local/apache2/conf/ssl/apache.crt', '/usr/local/apache2/conf/server.crt')
+    shutil.copy('/usr/local/apache2/conf/ssl/apache.key', '/usr/local/apache2/conf/server.key')
 
 def update_httpd_ssl_conf():
-    print("Updating httpd-ssl.conf with SSL settings...")
-    ssl_conf_content = """
+    block = """
 ServerName 192.168.2.2
 <IfModule ssl_module>
-    Listen 443
-    SSLPassPhraseDialog builtin
-    SSLSessionCache shmcb:/usr/local/apache2/logs/ssl_scache(512000)
-    SSLSessionCacheTimeout 300
-    SSLCertificateFile "/usr/local/apache2/conf/server.crt"
-    SSLCertificateKeyFile "/usr/local/apache2/conf/server.key"
-    <VirtualHost _default_:443>
-        ServerAdmin webmaster@localhost
-        DocumentRoot "/usr/local/apache2/htdocs"
-        ErrorLog "logs/ssl_error_log"
-        SSLEngine on
-    </VirtualHost>
+  Listen 443
+  SSLPassPhraseDialog  builtin
+  SSLSessionCache       shmcb:/usr/local/apache2/logs/ssl_scache(512000)
+  SSLSessionCacheTimeout  300
+  SSLMutex  file:/usr/local/apache2/logs/ssl_mutex
+  SSLCertificateFile "/usr/local/apache2/conf/server.crt"
+  SSLCertificateKeyFile "/usr/local/apache2/conf/server.key"
+  <VirtualHost _default_:443>
+    ServerAdmin webmaster@localhost
+    DocumentRoot "/usr/local/apache2/htdocs"
+    ErrorLog "logs/ssl_error_log"
+    CustomLog "logs/ssl_access_log" common
+    SSLEngine on
+  </VirtualHost>
 </IfModule>
 """
-    with open("httpd-ssl.conf", 'w') as file:
-        file.write(ssl_conf_content)
-    stdout, stderr = run_command(f"docker cp httpd-ssl.conf {container_name}:/usr/local/apache2/conf/extra/httpd-ssl.conf")
-    if stdout:
-        print(stdout)
-    if stderr:
-        print(stderr)
-    stdout, stderr = run_command(f"rm httpd-ssl.conf")
-    if stdout:
-        print(stdout)
-    if stderr:
-        print(stderr)
+    with open('/usr/local/apache2/conf/extra/httpd-ssl.conf', 'a') as f:
+        f.write(block)
 
-def include_ssl_conf():
-    print("Including httpd-ssl.conf in main configuration...")
-    stdout, stderr = run_command(f"docker exec {container_name} sh -c 'echo \"Include conf/extra/httpd-ssl.conf\" >> /usr/local/apache2/conf/httpd.conf'")
-    if stdout:
-        print(stdout)
-    if stderr:
-        print(stderr)
+def include_httpd_ssl_conf():
+    with open('/usr/local/apache2/conf/httpd.conf', 'a') as f:
+        f.write('\nInclude conf/extra/httpd-ssl.conf\n')
 
-def load_ssl_modules():
-    print("Loading SSL, socache_shmcb, and MPM modules...")
-    # Check if modules are already present
-    modules_line = (
-        "LoadModule ssl_module modules/mod_ssl.so\n"
-        "LoadModule socache_shmcb_module modules/mod_socache_shmcb.so\n"
-    )
-    
-    # Append the lines only if they are not already present
-    current_conf, _ = run_command(f"docker exec {container_name} cat /usr/local/apache2/conf/httpd.conf")
-    
-    if not all(module in current_conf for module in modules_line.splitlines()):
-        stdout, stderr = run_command(f"docker exec {container_name} sh -c 'echo \"{modules_line}\" >> /usr/local/apache2/conf/httpd.conf'")
-        if stdout:
-            print(stdout)
-        if stderr:
-            print(stderr)
-    else:
-        print("Modules are already loaded in httpd.conf")
-
+def load_modules():
+    with open('/usr/local/apache2/conf/httpd.conf', 'a') as f:
+        f.write('\nLoadModule ssl_module modules/mod_ssl.so\n')
+        f.write('LoadModule socache_shmcb_module modules/mod_socache_shmcb.so\n')
 
 def restart_apache():
-    print("Restarting Apache server...")
-    stdout, stderr = run_command(f"docker exec {container_name} apachectl restart")
-    print(stdout, stderr)
+    subprocess.run(['apachectl', 'restart'])
 
-def main():
+def configure_ssl_on_apache():
     rename_and_move_certificates()
     update_httpd_ssl_conf()
-    include_ssl_conf()
-    load_ssl_modules()
+    include_httpd_ssl_conf()
+    load_modules()
     restart_apache()
-    print("Apache server with SSL has been configured successfully.")
 
 if __name__ == "__main__":
-    main()
+    configure_ssl_on_apache()

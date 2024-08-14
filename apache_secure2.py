f0831ab84ac46a0abb41b4541aa8833631eea930
feezahlee <hafeezahlee3@gmail.com>
2024-08-14 13:41:16 +0800

    Update apache_secure2.py

--- apache_secure2.py
+++ apache_secure2.py
@@ -1,36 +1,52 @@
+import subprocess
+import fileinput
 import shutil
-import os
-import sys
 
 def rename_and_move_certificates():
-    # Define source and destination paths for the certificate and key
-    src_crt = os.path.expanduser('~/vrnetlab/csr/ssl/apache.crt')
-    dest_crt = '/usr/local/apache2/conf/server.crt'
-    src_key = os.path.expanduser('~/vrnetlab/csr/ssl/apache.key')
-    dest_key = '/usr/local/apache2/conf/server.key'
+    shutil.copy('/usr/local/apache2/conf/ssl/apache.crt', '/usr/local/apache2/conf/server.crt')
+    shutil.copy('/usr/local/apache2/conf/ssl/apache.key', '/usr/local/apache2/conf/server.key')
 
-    # Check if the source files exist
-    if not os.path.isfile(src_crt):
-        print(f"Error: Source certificate file does not exist: {src_crt}")
-        sys.exit(1)
-    if not os.path.isfile(src_key):
-        print(f"Error: Source key file does not exist: {src_key}")
-        sys.exit(1)
+def update_httpd_ssl_conf():
+    block = """
+ServerName 192.168.2.2
+<IfModule ssl_module>
+  Listen 443
+  SSLPassPhraseDialog  builtin
+  SSLSessionCache       shmcb:/usr/local/apache2/logs/ssl_scache(512000)
+  SSLSessionCacheTimeout  300
+  SSLMutex  file:/usr/local/apache2/logs/ssl_mutex
+  SSLCertificateFile "/usr/local/apache2/conf/server.crt"
+  SSLCertificateKeyFile "/usr/local/apache2/conf/server.key"
+  <VirtualHost _default_:443>
+    ServerAdmin webmaster@localhost
+    DocumentRoot "/usr/local/apache2/htdocs"
+    ErrorLog "logs/ssl_error_log"
+    CustomLog "logs/ssl_access_log" common
+    SSLEngine on
+  </VirtualHost>
+</IfModule>
+"""
+    with open('/usr/local/apache2/conf/extra/httpd-ssl.conf', 'a') as f:
+        f.write(block)
 
-    # Copy the certificate and key to the new locations
-    try:
-        shutil.copy(src_crt, dest_crt)
-        print(f"Certificate copied from {src_crt} to {dest_crt}")
-    except Exception as e:
-        print(f"Error copying certificate: {e}")
-        sys.exit(1)
+def include_httpd_ssl_conf():
+    with open('/usr/local/apache2/conf/httpd.conf', 'a') as f:
+        f.write('\nInclude conf/extra/httpd-ssl.conf\n')
 
-    try:
-        shutil.copy(src_key, dest_key)
-        print(f"Key copied from {src_key} to {dest_key}")
-    except Exception as e:
-        print(f"Error copying key: {e}")
-        sys.exit(1)
+def load_modules():
+    with open('/usr/local/apache2/conf/httpd.conf', 'a') as f:
+        f.write('\nLoadModule ssl_module modules/mod_ssl.so\n')
+        f.write('LoadModule socache_shmcb_module modules/mod_socache_shmcb.so\n')
+
+def restart_apache():
+    subprocess.run(['apachectl', 'restart'])
+
+def configure_ssl_on_apache():
+    rename_and_move_certificates()
+    update_httpd_ssl_conf()
+    include_httpd_ssl_conf()
+    load_modules()
+    restart_apache()
 
 if __name__ == "__main__":
-    rename_and_move_certificates()
+    configure_ssl_on_apache()

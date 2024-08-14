cb0ba30aa0796d87fb81e2a84b45eccc849a67d0
feezahlee <hafeezahlee3@gmail.com>
2024-08-14 16:28:01 +0800

    Update apache_secure2.py

--- apache_secure2.py
+++ apache_secure2.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def run_command(command):
-    """Function to run shell commands inside the Docker container."""
+    """Function to run shell commands."""
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     if result.returncode == 0:
         print(f"Command '{command}' executed successfully.")
@@ -9,43 +9,16 @@ def run_command(command):
         print(f"Error executing '{command}': {result.stderr}")
         raise Exception(result.stderr)
 
-def configure_ssl_certificates(container_name):
+def configure_ssl_on_apache():
     # Rename and move SSL certificate
-    run_command(f"docker exec {container_name} cp /usr/local/apache2/conf/ssl/apache.crt /usr/local/apache2/conf/server.crt")
+    run_command("cp /usr/local/apache2/conf/ssl/apache.crt /usr/local/apache2/conf/server.crt")
 
     # Rename and move SSL key
-    run_command(f"docker exec {container_name} cp /usr/local/apache2/conf/ssl/apache.key /usr/local/apache2/conf/server.key")
+    run_command("cp /usr/local/apache2/conf/ssl/apache.key /usr/local/apache2/conf/server.key")
 
-
-def configure_ssl_on_apache():
-    container_name = "clab-firstlab-apache-server"
-    httpd_conf_path = "/usr/local/apache2/conf/httpd.conf"
+    # Update httpd-ssl.conf with SSL settings
     ssl_conf_path = "/usr/local/apache2/conf/extra/httpd-ssl.conf"
-    index_html_path = "/usr/local/apache2/htdocs/index.html"
-
-    # Define the necessary modules and settings
-    mpm_module = "LoadModule mpm_event_module modules/mod_mpm_event.so"
-    ssl_module = "LoadModule ssl_module modules/mod_ssl.so"
-    socache_module = "LoadModule socache_shmcb_module modules/mod_socache_shmcb.so"
-    include_directive = "Include conf/extra/httpd-ssl.conf"
-    
-    # Generate httpd.conf content
-    httpd_conf_content = f"""
-{mpm_module}
-{ssl_module}
-{socache_module}
-{include_directive}
-"""
-
-    # Write the generated content to httpd.conf
-    with open("httpd.conf", "w") as httpd_conf_file:
-        httpd_conf_file.write(httpd_conf_content)
-    
-    run_command(f"docker cp httpd.conf {container_name}:{httpd_conf_path}")
-
-    # Write SSL configuration
-    with open("httpd-ssl.conf", "w") as ssl_conf_file:
-        ssl_conf_file.write("""
+    ssl_conf_content = """
 ServerName 192.168.2.2
 <IfModule ssl_module>
     Listen 443
@@ -55,25 +28,47 @@ ServerName 192.168.2.2
     SSLMutex  file:/usr/local/apache2/logs/ssl_mutex
     SSLCertificateFile "/usr/local/apache2/conf/server.crt"
     SSLCertificateKeyFile "/usr/local/apache2/conf/server.key"
-    <VirtualHost _default_:443>
+<VirtualHost _default_:443>
         ServerAdmin webmaster@localhost
         DocumentRoot "/usr/local/apache2/htdocs"
         ErrorLog "logs/ssl_error_log"
         CustomLog "logs/ssl_access_log" common
         SSLEngine on
-    </VirtualHost>
+</VirtualHost>
 </IfModule>
-""")
-    run_command(f"docker cp httpd-ssl.conf {container_name}:{ssl_conf_path}")
+"""
+
+    # Append SSL configuration to httpd-ssl.conf
+    with open(ssl_conf_path, "a") as ssl_conf_file:
+        ssl_conf_file.write(ssl_conf_content)
+
+    # Include httpd-ssl.conf in main configuration if not already included
+    httpd_conf_path = "/usr/local/apache2/conf/httpd.conf"
+    include_directive = "Include conf/extra/httpd-ssl.conf"
+    with open(httpd_conf_path, "r+") as httpd_conf_file:
+        lines = httpd_conf_file.readlines()
+        if include_directive not in lines:
+            httpd_conf_file.write(f"\n{include_directive}\n")
+
+    # Load SSL and socache_shmcb modules if not already loaded
+    ssl_module = "LoadModule ssl_module modules/mod_ssl.so"
+    socache_module = "LoadModule socache_shmcb_module modules/mod_socache_shmcb.so"
+    with open(httpd_conf_path, "r+") as httpd_conf_file:
+        lines = httpd_conf_file.readlines()
+        if ssl_module not in lines:
+            httpd_conf_file.write(f"\n{ssl_module}\n")
+        if socache_module not in lines:
+            httpd_conf_file.write(f"{socache_module}\n")
 
     # Update index.html content
-    with open("index.html", "w") as index_html_file:
-        index_html_file.write("""
+    index_html_path = "/usr/local/apache2/htdocs/index.html"
+    index_html_content = """
 <html>
   <body>
     <h1>Hello I am Yan Yan :))</h1>
     <h2 style="font-family: Arial, sans-serif;">The previous 'It works!' has been changed to this!</h2>
     <h3 style="color: blue;">This webpage has been changed by running Ansible Playbooks from Jenkins.</h3>
+
     <br><br><br>
     <p>Image:</p>
     <img src="https://www.tp.edu.sg/ecerts/images/banner.jpg" alt="Dog Image" width="500" height="300">
@@ -84,13 +79,14 @@ ServerName 192.168.2.2
     <p>All images and videos used are free of copyright and available for public use.</p>
   </body>
 </html>
-""")
-    run_command(f"docker cp index.html {container_name}:{index_html_path}")
+"""
+
+    # Write new content to index.html
+    with open(index_html_path, "w") as index_html_file:
+        index_html_file.write(index_html_content)
 
     # Restart Apache to apply changes
-    run_command(f"docker exec {container_name} apachectl restart")
+    run_command("apachectl restart")
 
 if __name__ == "__main__":
     configure_ssl_on_apache()
-
-    configure_ssl_certificates(container_name)
